00f9dc08a96e1bc762ee4326bc1ebbae3342e65f
csprashant <csprashantphuse@gmail.com>
2022-02-09 16:04:42 +0530

    first commit

---
+++ src/main/java/com/example/demo/moedl/Student.java
@@ -0,0 +1,19 @@
+package com.example.demo.moedl;
+
+public class Student {
+	private String name;
+	private String add;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+
+}
