e71e988e7bd1e186d76695d84450295a8c7fcdb4
csprashant <csprashantphuse@gmail.com>
2022-02-09 16:24:29 +0530

    testcases

--- src/test/java/com/example/demo/JenkinsDemoWithstipetestApplicationTests.java
+++ src/test/java/com/example/demo/JenkinsDemoWithstipetestApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JenkinsDemoWithstipetestApplicationTests {
+	private static Logger loger=org.slf4j.LoggerFactory.getLogger(JenkinsDemoWithstipetestApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		loger.info("Test case executed==============================================");
+		assertEquals(true,true);
 	}
 
 }
